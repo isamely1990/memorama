4e5c71ff5c49b42cc89ec23de7138b68b271695c
mely <meli.bermudez@yahoo.es>
2019-09-17 19:04:04 -0300

    refact maqueta, agregando funcionalidad para distribuir aleatoriamente las cartas

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -6,13 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards.component.sass']
 })
 export class CardsComponent implements OnInit {
+  cards =  [
+    1,2,3,4,5,6,7,8,9,10
+  ];
+  cardsCopy =  [
+    1,2,3,4,5,6,7,8,9,10
+  ];
   constructor() { }
   status: boolean = false;
-  clickEvent(){
+  clickEvent(e){
       console.log('click', this.status)
-      this.status = !this.status;       
+      const target = e.target
+      console.log(target)
+      const idAttr = target.attributes.id;
+      console.log(idAttr)
+      this.status = !this.status;    
+      
   }
   ngOnInit() {
+    this.cards.sort(function() {return Math.random() - 0.5});
+    this.cardsCopy.sort(function() {return Math.random() - 0.5});
   }
 
 }
